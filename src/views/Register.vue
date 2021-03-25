a53fc5631eea19e73874aacc6618106bcc7bd042
Julian Grisales <jgrisales@gmail.com>
2021-03-25 14:57:38 +0100

    Register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,23 @@
 <template>
   <BasicLayout>
-    <h1>Sign up</h1>
-    <router-link to="/login">Sign in</router-link>
+    <div class="register">
+      <h2>User register</h2>
+      <form class="ui form">
+        <div class="field">
+          <input type="text" placeholder="John Doe" />
+        </div>
+        <div class="field">
+          <input type="email" placeholder="john@example.com" />
+        </div>
+        <div class="field">
+          <input type="password" placeholder="Password" />
+        </div>
+        <button type="submit" class="ui button fluid primary">
+          Create account
+        </button>
+      </form>
+      <router-link to="/login">Sign in</router-link>
+    </div>
   </BasicLayout>
 </template>
 
@@ -18,4 +34,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.register {
+  text-align: center;
+  > h2 {
+    margin: 50px 0 30px 0;
+  }
+
+  .ui.form {
+    max-width: 300px !important;
+    margin: 0 auto;
+    margin-bottom: 10px;
+
+    input.error {
+      border-color: #faa;
+      background-color: #ffeded;
+    }
+  }
+}
 </style>
