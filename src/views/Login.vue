7100e4c9595607a1b2aef905dc9bc68d24749c02
Julian Grisales <jgrisales@gmail.com>
2021-03-25 16:35:55 +0100

    Login form validate and recovery data

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,14 +2,30 @@
   <BasicLayout>
     <div class="login">
       <h2>Sign in</h2>
-      <form class="ui form">
+      <form class="ui form" @submit.prevent="login">
         <div class="field">
-          <input type="text" placeholder="username" />
+          <input
+            type="text"
+            placeholder="username"
+            v-model="formData.username"
+            :class="{ error: formError.identifier }"
+          />
         </div>
         <div class="field">
-          <input type="password" placeholder="password" />
+          <input
+            type="password"
+            placeholder="password"
+            v-model="formData.password"
+            :class="{ error: formError.password }"
+          />
         </div>
-        <button type="submit" class="ui button fluid primary">Sign in</button>
+        <button
+          type="submit"
+          class="ui button fluid primary"
+          :class="{ loading }"
+        >
+          Sign in
+        </button>
       </form>
       <router-link to="/register">Create account</router-link>
     </div>
@@ -17,6 +33,8 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import * as Yup from "yup";
 import BasicLayout from "../layouts/BasicLayout";
 
 export default {
@@ -24,6 +42,39 @@ export default {
 
   components: {
     BasicLayout,
+  },
+
+  setup(props) {
+    let formData = ref({});
+    let formError = ref({});
+    let loading = ref(false);
+
+    const schemaForm = Yup.object().shape({
+      identifier: Yup.string().required(true), // Strapi requires the username like identifier
+      password: Yup.string().required(true),
+    });
+
+    const login = async () => {
+      formError.value = {};
+      loading.value = true;
+
+      try {
+        await schemaForm.validate(formData.value, { abortEarly: false });
+      } catch (error) {
+        error.inner.forEach((err) => {
+          formError.value[err.path] = err.message;
+        });
+      }
+
+      loading.value = false;
+    };
+
+    return {
+      formData,
+      formError,
+      loading,
+      login,
+    };
   },
 };
 </script>
