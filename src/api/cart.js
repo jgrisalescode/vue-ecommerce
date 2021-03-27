d73fa9c778ff2b545d5222a26132965770cc3404
Julian Grisales <jgrisales@gmail.com>
2021-03-27 12:28:35 +0100

    Cart add several products in localStorage

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,11 +1,20 @@
 import { API_URL, PRODUCTS } from '../utils/constants'
 
 const addProductCartApi = (productId) => {
-  const products = []
+  const products = getCartApi()
   products.push(productId)
   localStorage.setItem(PRODUCTS, JSON.stringify(products))
 }
 
+const getCartApi = () => {
+  const products = localStorage.getItem(PRODUCTS)
+
+  if (!products) return [];
+
+  return JSON.parse(products)
+}
+
 export {
-  addProductCartApi
+  addProductCartApi,
+  getCartApi
 }
